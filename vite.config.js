b0741e6f839d87d0e2bb97b4f91699d72c662fc5
vgf02140 <vgf021404@gmail.com>
2026-08-28 08:44:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:3001"
-    }
-  }
-});
+  base: '/Estuda.AI/',
+})
